abf40bb807d8c44ca996c77ce2a699f0b3f4575a
berking27 <berkin.koca01@gmail.com>
2024-03-11 22:32:39 +0300

    3 column LazyVGrid View implemented. and FrameworkTitleView created.

--- Apple-Frameworks/Screens/FrameworkGridView.swift
+++ Apple-Frameworks/Screens/FrameworkGridView.swift
@@ -8,17 +8,45 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+     let columns: [GridItem] = [GridItem(.flexible()),
+                                GridItem(.flexible()),
+                                GridItem(.flexible())]
+     
+     var body: some View {
+          
+          LazyVGrid(columns: columns) {
+               FrameworkTitleView(name: "App Clips", iconImage: Image(.appClip))
+               FrameworkTitleView(name: "ArKit", iconImage: Image(.arkit))
+               FrameworkTitleView(name: "CarPlay", iconImage: Image(.carplay))
+               
+               FrameworkTitleView(name: "Catalyst", iconImage: Image(.catalyst))
+               FrameworkTitleView(name: "ClassKit", iconImage: Image(.classkit))
+               FrameworkTitleView(name: "CloudKit", iconImage: Image(.cloudkit))
+               
+               
+          }
+     }
+}
+
+struct FrameworkTitleView: View {
+     let name: String
+     let iconImage: Image
+     
+     var body: some View {
+          VStack {
+               iconImage
+                    .resizable()
+                    .frame(width: 92, height: 92)
+               
+               Text(name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .scaledToFit()
+                    .minimumScaleFactor(0.6)
+          }
+     }
 }
 
 #Preview {
-    FrameworkGridView()
+     FrameworkGridView()
 }

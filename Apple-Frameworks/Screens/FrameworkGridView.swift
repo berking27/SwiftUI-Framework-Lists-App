94b36104550051a2c8eeac077d4d728df678b98b
berking27 <berkin.koca01@gmail.com>
2024-03-11 22:57:11 +0300

    Grid View completed. Re-indexing some pages

--- Apple-Frameworks/Screens/FrameworkGridView.swift
+++ Apple-Frameworks/Screens/FrameworkGridView.swift
@@ -8,45 +8,47 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
-     let columns: [GridItem] = [GridItem(.flexible()),
-                                GridItem(.flexible()),
-                                GridItem(.flexible())]
-     
-     var body: some View {
-          
-          LazyVGrid(columns: columns) {
-               FrameworkTitleView(name: "App Clips", iconImage: Image(.appClip))
-               FrameworkTitleView(name: "ArKit", iconImage: Image(.arkit))
-               FrameworkTitleView(name: "CarPlay", iconImage: Image(.carplay))
-               
-               FrameworkTitleView(name: "Catalyst", iconImage: Image(.catalyst))
-               FrameworkTitleView(name: "ClassKit", iconImage: Image(.classkit))
-               FrameworkTitleView(name: "CloudKit", iconImage: Image(.cloudkit))
-               
-               
-          }
-     }
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+        
+    }
 }
 
 struct FrameworkTitleView: View {
-     let name: String
-     let iconImage: Image
-     
-     var body: some View {
-          VStack {
-               iconImage
-                    .resizable()
-                    .frame(width: 92, height: 92)
-               
-               Text(name)
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .scaledToFit()
-                    .minimumScaleFactor(0.6)
-          }
-     }
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 92, height: 92)
+            
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
 }
 
+
+
 #Preview {
-     FrameworkGridView()
+    FrameworkGridView()
 }
